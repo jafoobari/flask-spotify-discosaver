68d045d5d1e2da731eda14ae30991b3e23e4659c
Jabari A <Jabari.Allen@gmail.com>
2018-02-18 18:32:36 -0600

    Remove ignored files and add refresh_token to database

--- app/models.py
+++ app/models.py
@@ -4,7 +4,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
-    token = db.Column(db.String(128))
+    token = db.Column(db.String(256))
+    refresh_token = db.Column(db.String(256))
 
     def __repr__(self):
         return '<User {}>'.format(self.username)    
