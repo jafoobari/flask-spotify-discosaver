c98946af624791d59b9845125a6935cca2bcca18
Jabari A <Jabari.Allen@gmail.com>
2018-06-02 21:32:28 -0500

    Add monthly/weekly saved booleans to user model.

--- dw_saver/models.py
+++ dw_saver/models.py
@@ -5,6 +5,8 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
+    weekly_scheduled = db.Column(db.Boolean, default=False)
+    monthly_scheduled = db.Column(db.Boolean, default=False)
     access_token = db.Column(db.String(256))
     refresh_token = db.Column(db.String(256))
     token_expires_at = db.Column(db.Integer)
