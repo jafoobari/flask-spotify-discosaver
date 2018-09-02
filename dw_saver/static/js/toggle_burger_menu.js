58e5a21cc17acbb72730f145552880c2d8053b57
hyzypg <hyzypg@MacBook-Pro-3.local>
2018-09-01 18:39:48 -0600

    Improve mobile burger menu.

--- dw_saver/static/js/toggle_burger_menu.js
+++ dw_saver/static/js/toggle_burger_menu.js
@@ -3,6 +3,8 @@
 	var menu = document.querySelector('#'+burger.dataset.target);
 	burger.addEventListener('click', function() {
 		burger.classList.toggle('is-active');
+		burger.classList.toggle('has-background-success');
 		menu.classList.toggle('is-active');
+		menu.classList.toggle('box');
 	});
 })();
