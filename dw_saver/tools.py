3b74369aacf2b47975f445eaf0c57cda78808229
hyzypg <hyzypg@MacBook-Pro-4.local>
2021-06-24 23:50:30 -0500

    Comment out

--- dw_saver/tools.py
+++ dw_saver/tools.py
@@ -44,8 +44,8 @@ def find_playlist_by_name(user, name):
     playlist_index = dict_index_by_key(playlists['items'], 'name', name)
 
     while playlist_index is None and playlists['next']:
-        offset = playlists["offset"]
-        print(f'No luck with {offset} - {offset+50}')
+        # offset = playlists["offset"]
+        # print(f'No luck with {offset} - {offset+50}')
         playlists = sp.next(playlists)
         playlist_index = dict_index_by_key(playlists['items'], 'name', name)
 
